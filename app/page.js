d66f11a43c8dd3196daec1d47a1e0166967bf40f
tienphap1405 <tienphap1405@gmail.com>
2024-11-26 16:56:57 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <div>
+        <h1>HELLO</h1>
         <h1>Project Topic: My Anime Vault</h1>
         <h3>Team Member</h3>
         <ul>
