c0bc12bcb299b5c7488b97eef435b09fd770851b
Lunaroy13 <romano_luna@hotmail.com>
2024-12-10 12:54:41 -0700

    See More... category functionality  and  adding category buttons.

--- app/components/category-button.js
+++ app/components/category-button.js
@@ -1,10 +1,11 @@
-export default function CategoryButton({name, category, handleSetCategory, longForm}) {
+export default function CategoryButton({name, prevCategory, newCategory, handleSetCategory, longForm}) {
     return (
         longForm && (
             <div>
                 <button 
-                    className="bg-cyan-500 text-white font-bold py-2 px-4 rounded mb-4 " 
-                    onClick={() => handleSetCategory(category)}>
+                    className={`text-black font-bold py-2 px-4 rounded mb-4 ${ newCategory === prevCategory ? "bg-amber-500 font-bold" : " hover:bg-amber-500" } `} 
+                    disabled={prevCategory === newCategory} 
+                    onClick={() => handleSetCategory(newCategory)}>
                     {name}
                 </button>
             </div>
